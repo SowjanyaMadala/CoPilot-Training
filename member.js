f4f502f0c4ee55fe6ddd013508f4d2afcb7a37f9
SowjanyaMadala <49454710+SowjanyaMadala@users.noreply.github.com>
2024-09-10 17:55:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,137 @@
+// Remove the signature declaration
+function skillsMember() {
+    var skills = [
+        {
+            type: 'Frontend',
+            list: [
+                'HTML',
+                'CSS',
+                'JavaScript',
+                'jQuery',
+                'Sass',
+                'React',
+                'Redux',
+                'Angular',
+                'Vue',
+                'Webpack',
+                'Babel'
+            ]
+        },
+        {
+            type: 'Backend',
+            list: [
+                'Node',
+                'Express',
+                'Koa',
+                'Meteor',
+                'RESTful API',
+                'GraphQL',
+                'MongoDB',
+                'MySQL',
+                'PostgreSQL',
+                'Firebase',
+                'Socket.io'
+            ]
+        },
+        {
+            type: 'Mobile',
+            list: [
+                'React Native',
+                'Flutter',
+                'iOS',
+                'Android',
+                'Swift',
+                'Kotlin'
+            ]
+        },
+        {
+            type: 'DevOps',
+            list: [
+                'Docker',
+                'Jenkins',
+                'Travis CI',
+                'Circle CI',
+                'AWS',
+                'Heroku',
+                'Netlify',
+                'Vercel',
+                'Digital Ocean',
+                'Google Cloud',
+                'Azure'
+            ]
+        },
+        {
+            type: 'Design',
+            list: [
+                'Photoshop',
+                'Illustrator',
+                'Sketch',
+                'Figma',
+                'Zeplin'
+            ]
+        },
+        {
+            type: 'Data',
+            list: [
+                'D3.js',
+                'TensorFlow',
+                'Pandas',
+                'Scikit-learn',
+                'Numpy',
+                'Matplotlib',
+                'MapReduce',
+                'Spark',
+                'Hadoop',
+                'Kafka',
+                'Cassandra'
+            ]
+        },
+        {
+            type: 'Testing',
+            list: [
+                'Jest',
+                'Mocha',
+                'Chai',
+                'Enzyme',
+                'Cypress',
+                'Selenium',
+                'Appium',
+                'Jasmine',
+                'Karma',
+                'Protractor'
+            ]
+        },
+        {
+            type: 'Security',
+            list: [
+                'OAuth',
+                'JWT',
+                'Passport',
+                'bcrypt',
+                'OpenSSL',
+                'SSL',
+                'TLS',
+                'Firewall',
+                'CORS',
+                'CSRF',
+                'XSS'
+            ]
+        },
+        {
+            type: 'Others',
+            list: [
+                'Git',
+                'GitHub',
+                'GitLab',
+                'Bitbucket',
+                'Jira',
+                'Trello',
+                'Asana',
+                'Slack',
+                'Discord',
+                'Zoom',
+                'Skype'
+            ]
+        }
+    ]
+}
